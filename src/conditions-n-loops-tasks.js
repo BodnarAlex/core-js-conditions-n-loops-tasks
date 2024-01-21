bdbac4e824020a1b6566e5ccd74900bc2cbd799c
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-22 00:53:46 +0200

    feat: add solute convert to roman

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -110,8 +110,54 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let ones = '';
+  let res = '';
+  let newNum = num % 10;
+  switch (newNum) {
+    case 1:
+      ones = 'I';
+      break;
+    case 2:
+      ones = 'II';
+      break;
+    case 3:
+      ones = 'III';
+      break;
+    case 4:
+      ones = 'IV';
+      break;
+    case 5:
+      ones = 'V';
+      break;
+    case 6:
+      ones = 'VI';
+      break;
+    case 7:
+      ones = 'VII';
+      break;
+    case 8:
+      ones = 'VIII';
+      break;
+    case 9:
+      ones = 'IX';
+      break;
+    default:
+      break;
+  }
+  if (num > 9) {
+    newNum = (num - newNum) / 10;
+    if (newNum === 1) {
+      res = 'X';
+    }
+    if (newNum === 2) {
+      res = 'XX';
+    }
+    if (newNum === 3) {
+      res = 'XXX';
+    }
+  }
+  return res + ones;
 }
 
 /**
